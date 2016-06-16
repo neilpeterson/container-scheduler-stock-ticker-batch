abc25102ea31d3b8f57f3803e141f8fbb0b13f54
Neil Peterson <neilpeterson@outlook.com>
2016-06-15 22:11:39 -0700

    Update stock-report.py

--- stock-report.py
+++ stock-report.py
@@ -29,6 +29,7 @@ email = args.email
 # get stock quote and populate list
 for symbol in symbols:
     r = requests.get(stockurl + symbol)
+    print(r.text)
     s = json.loads(r.text[18:-1])
     price = (s['LastPrice'])
     l.append(symbol + ' = ' + str(s['LastPrice']) + '\n')  
